15c473467aa3b2164d290bcd09cc8ea81e0690c2
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-01 08:32:23 -0600

    primera version

---
+++ BucleWhile.py
@@ -0,0 +1,12 @@
+#Bucle While
+import math
+
+numero = int(input("Digite un número"))
+
+while numero<0:
+    print("Error, el número debe de ser positivo")
+    numero = int(input("Digite un número"))
+
+print(f"La raíz del número es: {(math.sqrt(numero)):.2f}")
+
+#Carolina EM
